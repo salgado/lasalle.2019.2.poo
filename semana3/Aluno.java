80fcf337ad4b29509d1de93b10eb4d1a3477a105
Alex Vanderlei Salgado <28008053@ACADEMICO>
2019-08-26 21:30:48 -0300

    semana 3 - parte 1

--- semana3/Aluno.java
+++ semana3/Aluno.java
@@ -4,6 +4,26 @@ public class Aluno {
 	private String matricula;
 	private String nome;
 	private String curso;
+	private String faculdade;
+	
+	//construtor
+	public Aluno()
+	{
+		this.faculdade = "Unilasalle-RJ";
+	}
+	
+	public Aluno(String fac)
+	{
+		this.faculdade = fac;
+	}
+	
+	//metodos getters/setters
+	public String getFaculdade() {
+		return faculdade;
+	}
+	public void setFaculdade(String faculdade) {
+		this.faculdade = faculdade;
+	}
 	public String getMatricula() {
 		return matricula;
 	}
@@ -20,7 +40,11 @@ public class Aluno {
 		return curso;
 	}
 	public void setCurso(String curso) {
-		this.curso = curso;
+		
+		if (curso.equalsIgnoreCase("si"))
+			this.curso = "Sistemas de Informação";
+		else
+			this.curso = curso;
 	}
 	
 	public void listar()
@@ -28,5 +52,6 @@ public class Aluno {
 		System.out.println("Matricula = " + this.getMatricula());
 		System.out.println("Nome = " + this.getNome());
 		System.out.println("Curso = " + this.getCurso());
+		System.out.println("Faculdade = " + this.getFaculdade());
 	}
 }
